4f6690056a4e1365aae0436ad9eaf215f4045c78
Dmitry <dnitievskiy@gmail.com>
2019-05-06 23:44:51 +0300

    changes

---
+++ 2 course/Программирование (JAVA)/Lab-3-C/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Line[] line = new Line[4];
+        line[0] = new Line(2.2,2.2,2.2,2.2);
+        line[1] = new Line(8.6,4.1,6.2,5.6);
+        line[2] = new Line(9.4,3.2,2.1,5.5);
+        line[3] = new Line(7.5,6.5,2.3,4.3);
+    }
+}
