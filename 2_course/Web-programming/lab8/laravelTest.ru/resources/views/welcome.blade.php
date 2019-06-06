47e421153ad4c6f29c1e60b36a064b70dd219d43
Dmitry <dnitievskiy@gmail.com>
2019-06-06 14:57:56 +0300

    changes

---
+++ 2_course/Web-programming/lab8/laravelTest.ru/resources/views/welcome.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:80" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 80;
+                font-family: 'Comic Sans';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 80px;
+                
+
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Nitievsky Dmitry</div>
+                <div class="title">Laravel Test</div>
+            </div>
+        </div>
+    </body>
+</html>
